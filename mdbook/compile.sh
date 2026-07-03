1b0434774205cbc00befbd18f259e085e051892d
RealKindOne <RealKindOne@users.noreply.github.com>
2026-07-03 16:51:43 -0400

    Update mdbook stuff

--- mdbook/compile.sh
+++ mdbook/compile.sh
@@ -5,6 +5,8 @@ cp ../commands/*.html src/commands/
 cp ../events/*.html src/events/
 cp ../other/*.html src/other/
 cp ../images/* src/images/
+cp ../dialog/* src/dialog/
+cp ../sockets/* src/sockets/
 cd src/commands
 
 for file in *.html; do   mv "$file" "${file%.html}.md"; done
@@ -21,6 +23,9 @@ sed -i 's|<head>||g' *.md
 sed -i 's|</head>||g' *.md
 sed -i 's|<p>||g' *.md
 sed -i 's|</p>||g' *.md
+sed -i 's|<P>||g' *.md
+# hload
+sed -i 's|\&copy|\&amp;copy|'g hload.md
 
 cd ..
 
@@ -40,6 +45,7 @@ sed -i 's|<head>||g' *.md
 sed -i 's|</head>||g' *.md
 sed -i 's|<p>||g' *.md
 sed -i 's|</p>||g' *.md
+sed -i 's|<P>||g' *.md
 
 cd ..
 
@@ -59,7 +65,7 @@ sed -i 's|<head>||g' *.md
 sed -i 's|</head>||g' *.md
 sed -i 's|<p>||g' *.md
 sed -i 's|</p>||g' *.md
-
+sed -i 's|<P>||g' *.md
 
 cd ..
 
@@ -79,7 +85,48 @@ sed -i 's|<head>||g' *.md
 sed -i 's|</head>||g' *.md
 sed -i 's|<p>||g' *.md
 sed -i 's|</p>||g' *.md
+sed -i 's|<P>||g' *.md
 
+# Fix warning...
+sed -i 's|<pre><code class="code">|```c|g' sendmessage.md
+sed -i 's|</code></pre>|```|g' sendmessage.md
 
 cd ..
+cd sockets
+
+for file in *.html; do   mv "$file" "${file%.html}.md"; done
+
+sed -i 's|<pre class ="code">|<pre><code class="code">|g' *.md
+sed -i 's|<pre class = "code">|<pre><code class="code">|g' *.md
+sed -i 's|</pre>|</code></pre>|g' *.md
+sed -i '/default.css/d' *.md
+sed -i 's|<html>||g' *.md
+sed -i 's|</html>||g' *.md
+sed -i 's|<body>||g' *.md
+sed -i 's|</body>||g' *.md
+sed -i 's|<head>||g' *.md
+sed -i 's|</head>||g' *.md
+sed -i 's|<p>||g' *.md
+sed -i 's|</p>||g' *.md
+sed -i 's|<P>||g' *.md
+cd ..
+
+
+cd dialog
+
+for file in *.html; do   mv "$file" "${file%.html}.md"; done
+
+sed -i 's|<pre class ="code">|<pre><code class="code">|g' *.md
+sed -i 's|<pre class = "code">|<pre><code class="code">|g' *.md
+sed -i 's|</pre>|</code></pre>|g' *.md
+sed -i '/default.css/d' *.md
+sed -i 's|<html>||g' *.md
+sed -i 's|</html>||g' *.md
+sed -i 's|<body>||g' *.md
+sed -i 's|</body>||g' *.md
+sed -i 's|<head>||g' *.md
+sed -i 's|</head>||g' *.md
+sed -i 's|<p>||g' *.md
+sed -i 's|</p>||g' *.md
+sed -i 's|<P>||g' *.md
 cd ..
--- mdbook/src/README.md
+++ mdbook/src/README.md
@@ -3,6 +3,7 @@
 
 This is a work in progress!
 
-There is a bug where empty lines in the examples will cause formatting issues.
+There is a bug where empty lines in the example code will cause formatting issues.
 
-All example code will need a '&nbsp;' at the empty line to fix this annoying
+All example code will need a html 'nbsp;' at the empty line to fix this annoying.
+
